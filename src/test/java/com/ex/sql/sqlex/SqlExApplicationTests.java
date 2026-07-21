aafb67a03cd2cebd3b14810a02d38c32df059162
Brandon <brandontjq@gmail.com>
2026-07-21 09:43:39 +0800

    Initial commit

---
+++ src/test/java/com/ex/sql/sqlex/SqlExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.sql.sqlex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
